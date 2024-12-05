fd1643eef234cbbe97637ef57f839bbae83cc06a
vinnietito <vincentnjuguna69@gmail.com>
2024-12-05 22:47:24 +0300

    Commits

---
+++ Solution.py
@@ -0,0 +1,7 @@
+# Import necessary libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Step 1: Load the dataset
+data_file = 'your_dataset.csv'  # Replace with your file name
+data = pd.read_csv(data_file)
